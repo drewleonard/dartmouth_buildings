60802bb4f2a4925eac725a37265aa05e9d453408
drewleonard <drew.n.leonard.19@dartmouth.edu>
2018-05-25 17:22:49 -0400

    reformatting to build through fully

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import render_template
 # PAGE ROUTING
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 # RUN APP FROM COMMAND LINE
 if __name__ == '__main__':
